1347f191073ddc0c97ac628695e409f1616bcbd1
Joellensilva <joellen.absilva@gmail.com>
2022-11-09 17:48:47 -0300

    Modificando pontuação

--- indice.go
+++ indice.go
@@ -47,6 +47,11 @@ func calcEasinessScore(meta coleta.Metadados) float64 {
 		"AMIGAVEL_PARA_RASPAGEM": 1, // será removido posteriormente
 		"RASPAGEM_DIFICULTADA":   0.5}
 
+	// Com a mudança do índice, o nível de acesso foi reduzido a 3 níveis:
+	// Acesso direto, raspagem dificultada e necessita simulação de usuário.
+	// A pontuação de "amigável para raspagem" foi modificada temporariamente durante o processo
+	// de remoção dessa forma de acesso a fim de que não afete o cálculo do índice.
+
 	score = score + calcStringCriteria(meta.Acesso.String(), options)
 	score = score + calcCriteria(meta.FormatoConsistente, 1)
 	score = score + calcCriteria(meta.EstritamenteTabular, 1)
